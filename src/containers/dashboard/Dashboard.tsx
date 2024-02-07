a0aea10b08c558d0bb5eb73205469814bf336be9
LP2851 <lucas.phillips28@hotmail.com>
2024-02-07 20:09:43 +0000

    Lint fix

--- src/containers/dashboard/Dashboard.tsx
+++ src/containers/dashboard/Dashboard.tsx
@@ -1,12 +1,10 @@
 import { memo } from "react";
 import { Link } from "react-router-dom";
-import { Theme, useTheme } from "../../context/theme/ThemeContext";
-import "./Dashboard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Dashboard.css";
 
 
 const DashboardContainer = () => {
-
   return <div className="dashboard-container">
     <div className="dashboard-grid">
       <Link className="dashboard-link-card" to="/blog">
