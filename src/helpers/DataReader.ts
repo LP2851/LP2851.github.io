5fed53e5ab7543c78219db98c8e5076564081fe4
LucasPhillips28 <lucas.phillips28@hotmail.com>
2024-02-07 23:01:49 +0000

    Fix: Read Blog Data from public storage json file (#16)
    * Get data from public/data/blogs.json
    
    * Add data for blogs page
    
    * Add spinner + data reader

---
+++ src/helpers/DataReader.ts
@@ -0,0 +1,4 @@
+export const fetchJsonData = async (location: string) => {
+  const response = await fetch(location);
+  return await response.json();
+}
--- src/helpers/PostData.ts
+++ src/helpers/PostData.ts
@@ -2,15 +2,11 @@ export type PostData = {
   title: string;
   content: Content;
   tags: string[];
-  controls: {
-    iFrameVideo?: string;
-  };
   date: string;
 };
 
 export enum ContentTypes {
   DEFAULT="default",
-  IFRAME_VIDEO="iframe-video",
 }
 
 export type Content = {
@@ -19,3 +15,32 @@ export type Content = {
     textContent?: string;
   };
 };
+
+export const formattedDate = (dateStr: string) => {
+  const date = new Date(dateStr)
+  const day = date.getDate();
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+
+  // Add ordinal suffix to the day
+  let dayWithSuffix;
+  switch (day % 10) {
+    case 1:
+      dayWithSuffix = day + "st";
+      break;
+    case 2:
+      dayWithSuffix = day + "nd";
+      break;
+    case 3:
+      dayWithSuffix = day + "rd";
+      break;
+    default:
+      dayWithSuffix = day + "th";
+  }
+
+  return `${dayWithSuffix} ${month} ${year}`;
+}
