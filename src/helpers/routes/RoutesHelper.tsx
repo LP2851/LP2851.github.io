91c4f4f412495ba5a84d2933cc8d4137be6ce089
LP2851 <lucas.phillips28@hotmail.com>
2024-02-17 23:53:53 +0000

    Add routes helpers + constants

---
+++ src/helpers/routes/RoutesHelper.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Dashboard } from "../../containers/dashboard/Dashboard";
+import { BlogPosts } from "../../containers/blog/BlogPosts";
+import { Projects } from "../../containers/projects/Projects";
+
+export enum ROUTE_PATHS {
+  DASHBOARD = "/",
+  BLOG = "/blog",
+  PROJECTS = "/projects"
+}
+
+export type RouteConfig = {
+  path: ROUTE_PATHS;
+  element?: JSX.Element;
+}
+
+export const ROUTES: RouteConfig[] = [
+  {
+    path: ROUTE_PATHS.DASHBOARD,
+    element: <Dashboard />,
+  },
+  {
+    path: ROUTE_PATHS.BLOG,
+    element: <BlogPosts />,
+  },
+  {
+    path: ROUTE_PATHS.PROJECTS,
+    element: <Projects />
+  }
+];
