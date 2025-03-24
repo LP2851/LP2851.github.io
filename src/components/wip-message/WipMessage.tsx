80a9b58efc637b3edbbe759ad9ac915ec770fbaa
LP2851 <lucas.phillips28@hotmail.com>
2025-03-24 20:48:43 +0000

    Update WIP component + add prettier to this projects info

--- src/components/wip-message/WipMessage.tsx
+++ src/components/wip-message/WipMessage.tsx
@@ -1,12 +1,13 @@
 import { memo } from "react";
 import { Card } from "../card/Card";
 import { Spinner } from "../spinner/Spinner";
+import "./WipMessage.css";
 
 const WipMessageComponent = () => {
   return (
     <Card>
-      <div>
-        <h3>This page is Work in Progress.</h3>
+      <div className="wip-message">
+        <h3>This page is a Work in Progress.</h3>
         <Spinner />
       </div>
     </Card>
